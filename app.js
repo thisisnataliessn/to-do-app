4dea7f79e8cd33b4b300077f5af2f4cd2c831d60
Natalie Zdan <natalie.zdan@gmail.com>
2019-01-02 13:23:31 -0600

    Implemented checkpoint changes but app not working

--- app.js
+++ app.js
@@ -1,48 +1,42 @@
 function onReady() {
-    const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
+    const toDos = [];
+    const addToDoForm = document.getElementById("addToDoForm");
+
+    function createNewToDo() {
+        const newToDoText = document.getElementById("newToDoText");
+        if (!newToDoText.value) {
+            return;
+        };
+        toDos.push({
+            title: newToDoText.value,
+            complete: false
+        });
+        newToDoText.value = "";
+        renderTheUI();
+    }
+
+    function renderTheUI() {
+        const toDoList = getElementById("toDoList");
+        toDoList.textContent = '';
+        toDos.forEach(function (toDo) {
+            const newLi = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+            //add the toDo's title text to newLI
+            newLi.textContent = toDo.title;
+            //update the DOM
+            toDoList.appendChild(newLi);
+            newLi.appendChild(checkbox);
+        });
+    }
     addToDoForm.addEventListener('submit', () => {
         event.preventDefault();
-        //get the text from form
-        let title = newToDoText.value;
-        //create a new li
-        let newLi = document.createElement('li');
-        //create a new input
-        let checkbox = document.createElement('input');
-        //set the input's type to checkbox
-        checkbox.type = 'checkbox';
-        //set the checkbox's class
-        checkbox.className = 'chk';
-        //set the title
-        newLi.textContent = title;
-        //attach the checkbox to the li
-        newLi.appendChild(checkbox);
-        //attach the li to the ul
-        toDoList.appendChild(newLi);
-        //set li's class
-        newLi.className = 'txt .mdl-list__item'
-        //empty the input field
-        newToDoText.value = "";
+        createNewToDo();
     })
-const deleteTask = document.getElementById('deleteTask');
-        deleteTask.addEventListener('click', () => {
-        let boxes = document.getElementsByClassName('chk');
-        let texts = document.getElementsByClassName('txt');
-        for (var i = 0; i < boxes.length; i++) {
-            box = boxes[i];
-            txt = texts[i]
-            if (box.checked) {
-                box.parentNode.removeChild(box);
-                txt.parentNode.removeChild(txt);
-            }
-        }
-    })
+
+    renderTheUI();
 }
 
-       
 window.onload = function () {
     onReady();
 }
-
-
